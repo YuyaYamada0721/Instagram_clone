5fc04af9dd5d7db576212a66000e6d0e1596aa1f
yuya yamada <yuya19920721@gmail.com>
2021-05-20 12:29:53 +0900

    generator scaffold の余分なファイル作られない設定

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,10 @@ module RailsExamClone3
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
   end
 end
