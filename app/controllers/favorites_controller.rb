270e48bb282690789da53af1ccaa254eff263dab
yuya yamada <yuya19920721@gmail.com>
2021-05-21 16:18:31 +0900

    favorite create  destroyの設定 一覧画面indexの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,20 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorite = Favorite.all
+  end
+
   def show
     @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+  end
+
+  def create
+    favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
   end
 end
